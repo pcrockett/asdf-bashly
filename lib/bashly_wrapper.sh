2ecb236cd31e6f3e59dc5c4bb83e81990bc07eb0
Phil Crockett <contact@philcrockett.com>
2024-11-11 11:27:29 +0100

    use bundler to setup environment properly

--- lib/bashly_wrapper.sh
+++ lib/bashly_wrapper.sh
@@ -4,9 +4,9 @@ set -Eeuo pipefail
 current_script_path=${BASH_SOURCE[0]}
 bin_dir="$(dirname "${current_script_path}")"
 install_dir="$(dirname "${bin_dir}")"
-gem_home="${install_dir}/gem_home"
+gem_home="${install_dir}/gem"
 
 GEM_HOME="${gem_home}" \
 	GEM_PATH="${gem_home}" \
 	PATH="${gem_home}/bin:${PATH}" \
-	"${gem_home}/bin/bashly" "${@}"
+	bundle exec bashly "${@}"
