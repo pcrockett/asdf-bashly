d5801ea945dee197d40d5b17a2f2c3c0dd9ba52c
Phil Crockett <contact@philcrockett.com>
2024-11-11 11:34:55 +0100

    fix bashly wrapper unable to find Gemfile

--- lib/bashly_wrapper.sh
+++ lib/bashly_wrapper.sh
@@ -9,4 +9,5 @@ gem_home="${install_dir}/gem"
 GEM_HOME="${gem_home}" \
 	GEM_PATH="${gem_home}" \
 	PATH="${gem_home}/bin:${PATH}" \
+	BUNDLE_GEMFILE="${install_dir}/Gemfile" \
 	bundle exec bashly "${@}"
