43e73b5968c9c55f46739095605d1c0a828f340b
Phil Crockett <contact@philcrockett.com>
2024-12-25 19:56:37 -0600

    fix: set GEM_HOME and GEM_PATH in bashly wrapper

--- lib/bashly_wrapper.sh
+++ lib/bashly_wrapper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-current_script_path=${BASH_SOURCE[0]}
+current_script_path="${BASH_SOURCE[0]}"
 bin_dir="$(dirname "${current_script_path}")"
 install_dir="$(dirname "${bin_dir}")"
 gem_home="${install_dir}/gem"
@@ -9,4 +9,6 @@ gem_home="${install_dir}/gem"
 PATH="${gem_home}/bin:${PATH}" \
 	BUNDLE_GEMFILE="${install_dir}/Gemfile" \
 	BUNDLE_PATH="${install_dir}/artifacts" \
+	GEM_HOME="${gem_home}" \
+	GEM_PATH="${gem_home}" \
 	bundle exec bashly "${@}"
