838f2ce0e98389dae68c97297c7150ce8c04b071
Phil Crockett <contact@philcrockett.com>
2025-09-17 10:21:07 +0200

    chore: remove dead code

--- lib/utils.bash
+++ lib/utils.bash
@@ -11,12 +11,6 @@ fail() {
 	exit 1
 }
 
-curl_opts=(-fsSL)
-
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
